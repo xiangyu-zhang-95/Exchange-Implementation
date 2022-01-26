18af5cc51b6e53dc803ef02823d9f9833c491454
Xiangyu Zhang <zhangxiangyu1995@outlook.com>
2022-01-25 20:09:19 -0500

    put initializatio into a separate cpp file

---
+++ src/ErrorMessage.cpp
@@ -0,0 +1,4 @@
+#include "ErrorMessage.h"
+
+std::ofstream* LogError::ptr_ofs = nullptr;
+const std::string LogError::file_name = "error_msg";
--- src/ErrorMessage.h
+++ src/ErrorMessage.h
@@ -20,8 +20,4 @@ private:
 
     static std::ofstream* ptr_ofs;
 };
-
-std::ofstream* LogError::ptr_ofs = nullptr;
-const std::string LogError::file_name = "error_msg";
-
 #endif
