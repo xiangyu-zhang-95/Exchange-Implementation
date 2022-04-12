ffe9443e83aa3304fe68ce90fb8506f3e68c921e
Xiangyu Zhang <zhangxiangyu1995@outlook.com>
2022-04-12 15:46:35 -0400

    pass new test: OrderMatch

--- src/OrderQueue.cpp
+++ src/OrderQueue.cpp
@@ -28,10 +28,10 @@ void OrderQueue::remove(std::list<VisibleOrder>::iterator it) {
 void OrderQueue::match_and_update(Order& order, std::unordered_map<O_Id,
                             std::pair<OrderQueue*, std::list<VisibleOrder>::iterator>>& order_map,
                                  std::deque<Feed>& dq_feed) {
-    assert(order.type != O_Type::ICEBERG);
+    // assert(order.type != O_Type::ICEBERG);
     while ((order.quant > 0) && (this->quantity > 0)) {
         std::list<VisibleOrder>::iterator ptr = this->peek();
-        assert(ptr->type != O_Type::ICEBERG);
+        // assert(ptr->type != O_Type::ICEBERG);
         if (ptr->quant > order.quant) {
             Feed f{F_Type::TRADE, this->price, order.quant, order.time};
             dq_feed.push_back(f);
@@ -39,6 +39,10 @@ void OrderQueue::match_and_update(Order& order, std::unordered_map<O_Id,
             ptr->quant -= order.quant;
             this->quantity -= order.quant;
             order.quant = 0;
+            if (order.type == O_Type::ICEBERG) {
+                order.quant = (order.display <= order.hidden? order.display: order.hidden);
+                order.hidden -= order.quant;
+            }
             continue;
         }
 
@@ -59,7 +63,36 @@ void OrderQueue::match_and_update(Order& order, std::unordered_map<O_Id,
             // update orderQueue
             this->quantity -= ptr->quant;
             dq_visible.erase(ptr);
+            continue;
         }
+
+        assert(ptr->type == O_Type::ICEBERG);
+        
+        // publish feed
+        Feed f{F_Type::TRADE, this->price, ptr->quant, order.time};
+        dq_feed.push_back(f);
+
+        // update order
+        order.quant -= ptr->quant;
+
+        // update order map
+        if (ptr->p_invisible->hidden == 0) {
+            assert(order_map.erase(ptr->id) == 1);
+            this->quantity -= ptr->quant;
+            dq_visible.erase(ptr);
+            continue;
+        }
+
+        // update orderQueue
+        this->quantity -= ptr->quant;
+
+        VisibleOrder replensh = *ptr;
+        replensh.quant = (replensh.display <= replensh.p_invisible->hidden? replensh.display: replensh.p_invisible->hidden);
+        replensh.p_invisible->hidden -= replensh.quant;
+        this->quantity += replensh.quant;
+
+        dq_visible.erase(ptr);
+        dq_visible.push_back(replensh);
     }           
 
 }
